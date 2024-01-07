4b1c3ade67d7bd8100c0a514b644e6300037fd46
gurwinder7735 <gurwinder7735@gmail.com>
2024-01-07 15:35:46 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,28 @@
+import { Metadata } from "next"
+import { Button } from "components/Button/Button"
+import { LP_GRID_ITEMS } from "lp-items"
+
+export const metadata: Metadata = {
+  title: "Next.js Enterprise Boilerplate",
+  twitter: {
+    card: "summary_large_image",
+  },
+  openGraph: {
+    url: "https://next-enterprise.vercel.app/",
+    images: [
+      {
+        width: 1200,
+        height: 630,
+        url: "https://raw.githubusercontent.com/Blazity/next-enterprise/main/project-logo.png",
+      },
+    ],
+  },
+}
+
+export default function Web() {
+  return (
+   <div>
+      This is the test page of my app
+   </div>
+  )
+}
